db0158719740ef393f680b399d77ccfb2c4a2ebb
inflamejl <inflamejl@126.com>
2022-06-19 17:06:47 +0800

    部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env sh
 
-# 确锟斤拷锟脚憋拷锟阶筹拷锟斤拷锟斤拷锟侥达拷锟斤拷
+# 确保脚本抛出遇到的错误
 set -e
 
-# 锟斤拷锟缴撅拷态锟侥硷拷 , yarn docs:build
+# 生成静态文件 , yarn docs:build
 npm run docs:build
 rm -rf ../blog/dist/*
 
-# 锟斤拷build锟斤拷锟缴碉拷dist目录锟斤拷锟斤拷锟斤拷锟斤拷一锟斤拷目录锟斤拷
+# 将build生成的dist目录拷贝至上一层目录中
 cp -rf dist ../blog/
 
-# 锟斤拷锟斤拷锟斤拷锟缴碉拷锟侥硷拷锟斤拷
+# 进入生成的文件夹
 cd ../blog/dist
 
-# git锟斤拷始锟斤拷锟斤拷每锟轿筹拷始锟斤拷锟斤拷影锟斤拷锟斤拷锟斤拷
+# git初始化，每次初始化不影响推送
 git init
 git add -A
 git commit -m 'deploy'
 git branch -M main
 
-# 锟斤拷锟斤拷锟斤拷锟揭拷锟斤拷锟� https://USERNAME.github.io
-git push -f git@github.com:it235/it235.github.io.git main
+# 如果你想要部署到 https://USERNAME.github.io
+git push -f git@github.com:my-GoldenAge/vuepress.git main
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ const sidebarConf = require('./config/sidebar')
 const pluginsConf = require('./config/plugins/index')
 module.exports = {
     //注意，此处需要填写你部署在nginx下的文件夹名称，如果是根目录，那么可以注释掉此行，注释掉后本地打开index.html无法访问
-    //base: "/dist/",
+    base: "/vuepress/",
     title: "MyGoldenAge",
     description: '那是我的黄金时代',
     dest: './dist',
